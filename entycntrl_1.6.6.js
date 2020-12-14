9af74ea6ecb621faf4e76669058323ef83e6bff2
ddtt786 <69110424+ddtt786@users.noreply.github.com>
2020-12-14 21:45:09 +0900

    Update and rename entycntrl_1.4.6.js to entycntrl_1.6.6.js

---
+++ entycntrl_1.6.6.js
@@ -0,0 +1,199 @@
+let __title
+let __id
+let __like
+let __view
+let __user
+const entry = {
+    ds: {
+        free: {
+            my : `https://playentry.org/api/discuss/find?username=${user.username}&title=&search_title=&sort=created&rows=0&page=1&category=free`,
+            get(getr) {
+                $.get('https://playentry.org/api/discuss/find?category=free', d => {
+                    __title = d.data[0].title;
+                    __id = d.data[0]._id;
+                    __like = d.data[0].likesLength;
+                    __view = d.data[0].visit;
+                    __user = d.data[0].owner;
+                    async: false
+                })
+                var ret
+                if(getr == "title"){ret = __title}
+                if(getr == "id"){ret = __id}
+                if(getr == "like"){ret = __like}
+                if(getr == "view"){ret = __view}
+                if(getr == "user"){ret = __user}
+                return ret
+            },
+            write(t,p) {
+                $.ajax({
+                    url:"https://playentry.org/api/discuss/",
+                    type:"POST",
+                    data:{
+                        content:p,
+                        title:t,
+                        groupNotice:false,
+                        images:[],
+                        category:"free"
+                    }
+                });
+            },
+            delete(del) {
+                $.get(entry.ds.free.my, d => {
+                    delfree = d.data[0]._id;
+                    if(del == undefined) {
+                        $.ajax({
+                            url: `https://playentry.org/api/discuss/${delfree}`,
+                            type: "DELETE",
+                        })
+                    }else{
+                        $.ajax({
+                            url: `https://playentry.org/api/discuss/${del}`,
+                            type: "DELETE",
+                        })
+                    }
+                })
+            }
+        },
+        qna: {
+            my : `https://playentry.org/api/discuss/find?username=${user.username}&title=&search_title=&sort=created&rows=0&page=1&category=qna`,
+            get(getr) {
+                $.get('https://playentry.org/api/discuss/find?category=qna', d => {
+                    __title = d.data[0].title;
+                    __id = d.data[0]._id;
+                    __like = d.data[0].likesLength;
+                    __view = d.data[0].visit;
+                    __user = d.data[0].owner;
+                    async: false
+                })
+                var ret
+                if(getr == "title"){ret = __title}
+                if(getr == "id"){ret = __id}
+                if(getr == "like"){ret = __like}
+                if(getr == "view"){ret = __view}
+                if(getr == "user"){ret = __user}
+                return ret
+            },
+            write(t,p) {
+                $.ajax({
+                    url:"https://playentry.org/api/discuss/",
+                    type:"POST",
+                    data:{
+                        content:p,
+                        title:t,
+                        groupNotice:false,
+                        images:[],
+                        category:"qna"
+                    }
+                });
+            },
+            delete(del) {
+                $.get(entry.ds.qna.my, d => {
+                    delfree = d.data[0]._id;
+                    if(del == undefined) {
+                        $.ajax({
+                            url: `https://playentry.org/api/discuss/${delfree}`,
+                            type: "DELETE",
+                        })
+                    }else{
+                        $.ajax({
+                            url: `https://playentry.org/api/discuss/${del}`,
+                            type: "DELETE",
+                        })
+                    }
+                })
+            }
+        },
+        tip: {
+            my : `https://playentry.org/api/discuss/find?username=${user.username}&title=&search_title=&sort=created&rows=0&page=1&category=tips`,
+            get(getr) {
+                $.get('https://playentry.org/api/discuss/find?category=tips', d => {
+                    __title = d.data[0].title;
+                    __id = d.data[0]._id;
+                    __like = d.data[0].likesLength;
+                    __view = d.data[0].visit;
+                    __user = d.data[0].owner;
+                    async: false
+                })
+                var ret
+                if(getr == "title"){ret = __title}
+                if(getr == "id"){ret = __id}
+                if(getr == "like"){ret = __like}
+                if(getr == "view"){ret = __view}
+                if(getr == "user"){ret = __user}
+                return ret
+            },
+            write(t,p) {
+                $.ajax({
+                    url:"https://playentry.org/api/discuss/",
+                    type:"POST",
+                    data:{
+                        content:p,
+                        title:t,
+                        groupNotice:false,
+                        images:[],
+                        category:"tip"
+                    }
+                });
+            },
+            delete(del) {
+                $.get(entry.ds.tip.my, d => {
+                    delfree = d.data[0]._id;
+                    if(del == undefined) {
+                        $.ajax({
+                            url: `https://playentry.org/api/discuss/${delfree}`,
+                            type: "DELETE",
+                        })
+                    }else{
+                        $.ajax({
+                            url: `https://playentry.org/api/discuss/${del}`,
+                            type: "DELETE",
+                        })
+                    }
+                })
+            }
+        }
+    },
+    project(pj) {
+        return {
+            like() {
+                $.ajax({
+                    url: `https://playentry.org/api/project/favorite/${pj}`,
+                    type: "POST",
+                    data: {
+                        targetSubject: "project", targetType: "individual"
+                    }
+                })
+            },
+            star() {
+                $.ajax({
+                    url: `https://playentry.org/api/project/favorite/${pj}`,
+                    type: "POST",
+                    data: {
+                        targetSubject: "project", targetType: "individual",
+                    }
+                })
+            },
+            save(saver) {
+                if(saver == undefined) {
+                    $.ajax({
+                        url: `https://playentry.org/api/project/${pj}`,
+                        type: "PUT",
+                        data: { "isopen": true, "group": [] },
+                        success: function (data) {
+                            console.log(data)
+                        }
+                    })
+                }else{
+                    $.ajax({
+                        url: `https://playentry.org/api/project/${pj}`,
+                        type: "PUT",
+                        data: { "isopen": true, "group": [] ,name: saver,},
+                        success: function (data) {
+                            console.log(data)
+                        }
+                    })
+                }
+            }
+        }
+    }
+}
